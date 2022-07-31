5ff7c3169a0735a4914c3a1ed7c80ac928467b05
KiranAnnadurai <110341833+KiranAnnadurai@users.noreply.github.com>
2022-07-31 22:32:18 +0530

    Update PatternTypeOne.java

--- PatternTypeOne.java
+++ PatternTypeOne.java
@@ -31,7 +31,7 @@ public class PatternTypeOne {
 		
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<n;j++) {
-				if(i==0 || i==n-1 || j==0 || j==n-1 || i+j <= (n-1)/2 || j-i >= (n-1)/2 )
+				if(i == 0 || i == n-1 || j == 0 || j == n-1 || i+j <= (n-1)/2 || j-i >= (n-1)/2 )
 					System.out.print("*");
 				else
 					System.out.print(" ");
